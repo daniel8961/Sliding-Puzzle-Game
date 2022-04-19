a3ab1d39912c080fda4b7f88e041831d01ddb3b1
Benny Lisangi <benilisangi2019@gmail.com>
2022-04-19 17:39:45 -0400

    finished

--- moving.js
+++ moving.js
@@ -1,53 +1,92 @@
-// const images =  [`numbers/image009.gif`,"numbers/image010.gif", "numbers/image011.gif",
-// "numbers/image012.gif", "numbers/image013.gif", "numbers/image014.gif", "numbers/image015.gif",
-// "numbers/image016.gif", "numbers/image017.gif", "numbers/image018.gif", "numbers/image019.gif",
-// "numbers/image020.gif","numbers/image021.gif", "numbers/image022.gif", "numbers/image023.gif",
-// };
+const numbers = [1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14, 15,""]
+let outputs = []
+window.onload = () =>{
+  const temp_arr = random_shuffle(numbers)
+  for (let i = 1; i<=16; i++){
+    if(temp_arr[i-1] ==""){
+    }
+    document.getElementById(`${i}`).innerHTML = temp_arr[i-1];
+  }
+  movable(temp_arr)
+}
 let row = 4; let col = 4;
 
 function isSolved(board){
   for (let i = 0; i < board.length - 1; i++) {
-    if (board[i] !== images[i].index) {
+    if (board[i] != numbers[i]) {
       return false;
     }
   }
   return true;
 }
 
-function clicked(board){
-  move(i,j,board);
+function random_shuffle(arr){
+  let curr = 0;
+  let randomIndex;
+  while(curr < arr.length){
+      randomIndex =Math.floor(Math.random()*curr);
+      [arr[randomIndex], arr[curr]] = [arr[curr], arr[randomIndex]];
+      curr +=1
+  }
+  return arr;
 }
-function swap(i, j, board) {
-  let temp = board[i];
-  board[i] = board[j];
-  board[j] = temp;
-}
-function move(i, j, board) {
-  let blank = findBlank();
-  let blank_col = blank % col;
-  let blank_row = floor(blank / row);
+function dragMouseDown(target, idx){
+  outputs = []
+  const content = document.getElementById(target).innerHTML;
+  document.getElementById(target).innerHTML = document.getElementById(idx).innerHTML;
+  document.getElementById(idx).innerHTML = content;
+  for (let i = 0; i<16; i++){
+    const elem = document.getElementById(`${i+1}`).innerHTML;
+    const element = document.getElementById(`${i+1}`) 
+    outputs.push(elem)
+    element.replaceWith(element.cloneNode(true))
+  }
+  if(isSolved(outputs)){
+    alert("You've completed the puzzle! Congrats!")
+  }
+  movable(outputs)
+  
 
-  // Double check valid move
-  if (isNeighbor(i, j, blank_col, blank_row)) {
-    swap(blank, i + j * col, board);
-  }
-  if(isSolved){
-    alert("You Won!");
-    break;
-  }
-}
-function findBlank(board) {
-  for (let i = 0; i < board.length; i++) {
-    if (board[i] == -1) return i;
-  }
-}
-function isNeighbor(x,y,x_final,y_final){
-  if (x !== x_final && y !== y_final) {
-    return false;
-  }
 
-  if (abs(x - x_final) == 1 || abs(y - y_final) == 1) {
-    return true;
+}
+
+function movable(arr){
+  outputs = [];
+   const index = arr.indexOf("")
+   document.getElementById(`${index+1}`).style.backgroundColor = 'white'
+   var positions = [];
+   if (index<12){
+     positions.push(index+4);
+   }
+   if (index>3){
+     positions.push(index-4)
+   }
+   if (index !==3 && index!==7 && index!==11 && index !==15){
+     positions.push(index+1)
+   }
+   if (index !==0 && index!==4 && index!==8 && index !==12){
+    positions.push(index-1)
   }
-  return false;
+  for(let i = 0; i<positions.length; i++){
+    const item = positions[i]
+    document.getElementById(`${item+1}`).onclick = () =>{
+      dragMouseDown((index+1),`${item+1}`)
+    }
+    document.getElementById(`${item+1}`).onmouseover = ()=>{
+      document.getElementById(`${item+1}`).style.backgroundColor = 'green';
+    }
+    document.getElementById(`${item+1}`).onmouseout = ()=>{
+      document.getElementById(`${item+1}`).style.backgroundColor ="white"
+    }
+  }
+}
+function reshuffle(){
+    const temp_arr = random_shuffle(numbers)
+    for (let i = 1; i<=16; i++){
+      document.getElementById(`${i}`).innerHTML = temp_arr[i-1];
+      const elem = document.getElementById(`${i}`);
+      elem.replaceWith(elem.cloneNode(true))
+    }
+    movable(temp_arr)
+  
 }
